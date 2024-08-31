64e22055e8db7edd8e5c1904a91cd50094ed1e8d
Rishini <nawodirishini88@gmail.com>
2024-08-31 11:20:36 +0530

    declare global constatnt definition

--- snake.py
+++ snake.py
@@ -1,1 +1,20 @@
 import pygame
+import sys
+import os
+import random 
+import math
+
+pygame.init()
+pygame.display.set_caption("snake game")
+pygame.font.init()
+random.speed()
+
+#declare global constatnt definition
+speed = 0.30
+SNAKE_SIZE = 9
+APPLE_SIZE = SNAKE_SIZE
+SEPERATION =10
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH= 800
+FPS = 25
+KEY = {"UP":1 ,"DOM":2 , "LEFT":3 ,"RIGHT":4}
