fe663e4367e39282895ecf070157b023b8c1c484
Rishini <nawodirishini88@gmail.com>
2024-08-31 11:27:02 +0530

    initialize screen and rescources

--- snake.py
+++ snake.py
@@ -18,3 +18,19 @@ SCREEN_HEIGHT = 600
 SCREEN_WIDTH= 800
 FPS = 25
 KEY = {"UP":1 ,"DOM":2 , "LEFT":3 ,"RIGHT":4}
+
+#initiallize screen
+screen = pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_HEIGHT),pygame.HWSURFACE)
+
+#HWSURFACE stands for hardware surface refere to using memory on the video card for storing
+
+#Resources
+score_font=pygame.font.Font(None,38)
+score_numb_font = pygame.font.Font(None,28)
+game_over_font = pygame.font.Font(None,46)
+play_again_font = score_numb_font
+score_msg = score_font.render("Score : ",1,pygame.Color("green"))
+score_msg_size = score_font.size("Score")
+background_color = pygame.Color(0,0,0)    # we will fill background color as black
+black = pygame.Color(0,0,0)
+
